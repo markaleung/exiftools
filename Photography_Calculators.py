a2753691fb9566af8c3dc062a83ff57696c60779
Mark Leung <markaleung@gmail.com>
2024-05-08 08:13:39 +0800

    Streamlit - Add lens weight to home page

--- Photography_Calculators.py
+++ Photography_Calculators.py
@@ -5,6 +5,8 @@ st.write(
 # Here are some calculators for
 - Exposure
     - Calculate based on aperture, ISO, and shutter speed
+- Lens Weight
+    - Calculate based on aperture and sensor size
 - Viewing Distance
     - Calculate megapixels, focal length, and pixel size
 
